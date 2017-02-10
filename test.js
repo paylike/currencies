95efe4862b044fda0b4316b01e1b846b1a9241be
Thomas Jensen <thomas@src.agency>
2017-02-11 00:03:44 +0200

    Use `tape` for tests

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 'use strict';
 
-var assert = require('assert');
+var test = require('tape');
 var currencies = require('./');
 
-assert.ok(Array.isArray(currencies));
+test(function( t ){
+	t.ok(Array.isArray(currencies));
 
-assert.deepStrictEqual(currencies.find(e => e.code === 'EUR'), {
-	code: 'EUR',
-	currency: 'Euro',
-	numeric: '978',
-	funding: true,
+	t.deepEqual(currencies.find(e => e.code === 'EUR'), {
+		code: 'EUR',
+		currency: 'Euro',
+		numeric: '978',
+		funding: true,
+	});
+
+	t.end();
 });
