6a7ff0c7c9fe6dcf88ee20348c5ac497f284335c
Thomas Jensen <thomas@src.agency>
2017-02-11 00:04:59 +0200

    Add more currencies, remove historical LTL (before: 47, now: 153)

--- index.js
+++ index.js
@@ -7,6 +7,31 @@ module.exports = [
 		numeric: '784',
 	},
 	{
+		code: 'AFN',
+		currency: 'Afghan afghani',
+		numeric: '971',
+	},
+	{
+		code: 'ALL',
+		currency: 'Albanian lek',
+		numeric: '008',
+	},
+	{
+		code: 'AMD',
+		currency: 'Armenian dram',
+		numeric: '051',
+	},
+	{
+		code: 'ANG',
+		currency: 'Netherlands Antillean guilder',
+		numeric: '532',
+	},
+	{
+		code: 'AOA',
+		currency: 'Angolan kwanza',
+		numeric: '973',
+	},
+	{
 		code: 'ARS',
 		currency: 'Argentine peso',
 		numeric: '032',
@@ -15,6 +40,11 @@ module.exports = [
 		code: 'AUD',
 		currency: 'Australian dollar',
 		numeric: '036',
+	},
+	{
+		code: 'AWG',
+		currency: 'Aruban florin',
+		numeric: '533',
 	},
 	{
 		code: 'AZN',
@@ -27,9 +57,44 @@ module.exports = [
 		numeric: '977',
 	},
 	{
+		code: 'BBD',
+		currency: 'Barbados dollar',
+		numeric: '052',
+	},
+	{
+		code: 'BDT',
+		currency: 'Bangladeshi taka',
+		numeric: '050',
+	},
+	{
 		code: 'BGN',
 		currency: 'Bulgarian lev',
 		numeric: '975',
+	},
+	{
+		code: 'BHD',
+		currency: 'Bahraini dinar',
+		numeric: '048',
+	},
+	{
+		code: 'BIF',
+		currency: 'Burundian franc',
+		numeric: '108',
+	},
+	{
+		code: 'BMD',
+		currency: 'Bermudian dollar',
+		numeric: '060',
+	},
+	{
+		code: 'BND',
+		currency: 'Brunei dollar',
+		numeric: '096',
+	},
+	{
+		code: 'BOB',
+		currency: 'Boliviano',
+		numeric: '068',
 	},
 	{
 		code: 'BRL',
@@ -37,14 +102,39 @@ module.exports = [
 		numeric: '986',
 	},
 	{
+		code: 'BSD',
+		currency: 'Bahamian dollar',
+		numeric: '044',
+	},
+	{
+		code: 'BTN',
+		currency: 'Bhutanese ngultrum',
+		numeric: '064',
+	},
+	{
+		code: 'BWP',
+		currency: 'Botswana pula',
+		numeric: '072',
+	},
+	{
 		code: 'BYR',
 		currency: 'Belarusian ruble',
 		numeric: '974',
 	},
 	{
+		code: 'BZD',
+		currency: 'Belize dollar',
+		numeric: '084',
+	},
+	{
 		code: 'CAD',
 		currency: 'Canadian dollar',
 		numeric: '124',
+	},
+	{
+		code: 'CDF',
+		currency: 'Congolese franc',
+		numeric: '976',
 	},
 	{
 		code: 'CHF',
@@ -62,9 +152,34 @@ module.exports = [
 		numeric: '156',
 	},
 	{
+		code: 'COP',
+		currency: 'Colombian peso',
+		numeric: '170',
+	},
+	{
+		code: 'CRC',
+		currency: 'Costa Rican colon',
+		numeric: '188',
+	},
+	{
+		code: 'CUP',
+		currency: 'Cuban peso',
+		numeric: '192',
+	},
+	{
+		code: 'CVE',
+		currency: 'Cape Verde escudo',
+		numeric: '132',
+	},
+	{
 		code: 'CZK',
-		currency: 'Czech Republic koruna',
+		currency: 'Czech koruna',
 		numeric: '203',
+	},
+	{
+		code: 'DJF',
+		currency: 'Djiboutian franc',
+		numeric: '262',
 	},
 	{
 		code: 'DKK',
@@ -78,9 +193,24 @@ module.exports = [
 		numeric: '214',
 	},
 	{
+		code: 'DZD',
+		currency: 'Algerian dinar',
+		numeric: '012',
+	},
+	{
 		code: 'EGP',
 		currency: 'Egyptian pound',
 		numeric: '818',
+	},
+	{
+		code: 'ERN',
+		currency: 'Eritrean nakfa',
+		numeric: '232',
+	},
+	{
+		code: 'ETB',
+		currency: 'Ethiopian birr',
+		numeric: '230',
 	},
 	{
 		code: 'EUR',
@@ -89,10 +219,55 @@ module.exports = [
 		funding: true,
 	},
 	{
+		code: 'FJD',
+		currency: 'Fiji dollar',
+		numeric: '242',
+	},
+	{
+		code: 'FKP',
+		currency: 'Falkland Islands pound',
+		numeric: '238',
+	},
+	{
 		code: 'GBP',
-		currency: 'British pound sterling',
+		currency: 'Pound sterling',
 		numeric: '826',
 		funding: true,
+	},
+	{
+		code: 'GEL',
+		currency: 'Georgian lari',
+		numeric: '981',
+	},
+	{
+		code: 'GHS',
+		currency: 'Ghanaian cedi',
+		numeric: '936',
+	},
+	{
+		code: 'GIP',
+		currency: 'Gibraltar pound',
+		numeric: '292',
+	},
+	{
+		code: 'GMD',
+		currency: 'Gambian dalasi',
+		numeric: '270',
+	},
+	{
+		code: 'GNF',
+		currency: 'Guinean franc',
+		numeric: '324',
+	},
+	{
+		code: 'GTQ',
+		currency: 'Guatemalan quetzal',
+		numeric: '320',
+	},
+	{
+		code: 'GYD',
+		currency: 'Guyanese dollar',
+		numeric: '328',
 	},
 	{
 		code: 'HKD',
@@ -100,9 +275,19 @@ module.exports = [
 		numeric: '344',
 	},
 	{
+		code: 'HNL',
+		currency: 'Honduran lempira',
+		numeric: '340',
+	},
+	{
 		code: 'HRK',
 		currency: 'Croatian kuna',
 		numeric: '191',
+	},
+	{
+		code: 'HTG',
+		currency: 'Haitian gourde',
+		numeric: '332',
 	},
 	{
 		code: 'HUF',
@@ -110,8 +295,13 @@ module.exports = [
 		numeric: '348',
 	},
 	{
+		code: 'IDR',
+		currency: 'Indonesian rupiah',
+		numeric: '360',
+	},
+	{
 		code: 'ILS',
-		currency: 'Israeli new sheqel',
+		currency: 'Israeli new shekel',
 		numeric: '376',
 	},
 	{
@@ -120,9 +310,29 @@ module.exports = [
 		numeric: '356',
 	},
 	{
+		code: 'IQD',
+		currency: 'Iraqi dinar',
+		numeric: '368',
+	},
+	{
+		code: 'IRR',
+		currency: 'Iranian rial',
+		numeric: '364',
+	},
+	{
 		code: 'ISK',
 		currency: 'Icelandic króna',
 		numeric: '352',
+	},
+	{
+		code: 'JMD',
+		currency: 'Jamaican dollar',
+		numeric: '388',
+	},
+	{
+		code: 'JOD',
+		currency: 'Jordanian dinar',
+		numeric: '400',
 	},
 	{
 		code: 'JPY',
@@ -130,14 +340,129 @@ module.exports = [
 		numeric: '392',
 	},
 	{
-		code: 'LTL',
-		currency: 'Lithuanian litas',
-		numeric: '440',
+		code: 'KES',
+		currency: 'Kenyan shilling',
+		numeric: '404',
+	},
+	{
+		code: 'KGS',
+		currency: 'Kyrgyzstani som',
+		numeric: '417',
+	},
+	{
+		code: 'KHR',
+		currency: 'Cambodian riel',
+		numeric: '116',
+	},
+	{
+		code: 'KMF',
+		currency: 'Comoro franc',
+		numeric: '174',
+	},
+	{
+		code: 'KPW',
+		currency: 'North Korean won',
+		numeric: '408',
+	},
+	{
+		code: 'KRW',
+		currency: 'South Korean won',
+		numeric: '410',
+	},
+	{
+		code: 'KWD',
+		currency: 'Kuwaiti dinar',
+		numeric: '414',
+	},
+	{
+		code: 'KYD',
+		currency: 'Cayman Islands dollar',
+		numeric: '136',
+	},
+	{
+		code: 'KZT',
+		currency: 'Kazakhstani tenge',
+		numeric: '398',
+	},
+	{
+		code: 'LAK',
+		currency: 'Lao kip',
+		numeric: '418',
+	},
+	{
+		code: 'LBP',
+		currency: 'Lebanese pound',
+		numeric: '422',
+	},
+	{
+		code: 'LKR',
+		currency: 'Sri Lankan rupee',
+		numeric: '144',
+	},
+	{
+		code: 'LRD',
+		currency: 'Liberian dollar',
+		numeric: '430',
+	},
+	{
+		code: 'LSL',
+		currency: 'Lesotho loti',
+		numeric: '426',
 	},
 	{
 		code: 'MAD',
 		currency: 'Moroccan dirham',
 		numeric: '504',
+	},
+	{
+		code: 'MDL',
+		currency: 'Moldovan leu',
+		numeric: '498',
+	},
+	{
+		code: 'MGA',
+		currency: 'Malagasy ariary',
+		numeric: '969',
+	},
+	{
+		code: 'MKD',
+		currency: 'Macedonian denar',
+		numeric: '807',
+	},
+	{
+		code: 'MMK',
+		currency: 'Myanmar kyat',
+		numeric: '104',
+	},
+	{
+		code: 'MNT',
+		currency: 'Mongolian tögrög',
+		numeric: '496',
+	},
+	{
+		code: 'MOP',
+		currency: 'Macanese pataca',
+		numeric: '446',
+	},
+	{
+		code: 'MRO',
+		currency: 'Mauritanian ouguiya',
+		numeric: '478',
+	},
+	{
+		code: 'MUR',
+		currency: 'Mauritian rupee',
+		numeric: '480',
+	},
+	{
+		code: 'MVR',
+		currency: 'Maldivian rufiyaa',
+		numeric: '462',
+	},
+	{
+		code: 'MWK',
+		currency: 'Malawian kwacha',
+		numeric: '454',
 	},
 	{
 		code: 'MXN',
@@ -150,10 +475,35 @@ module.exports = [
 		numeric: '458',
 	},
 	{
+		code: 'MZN',
+		currency: 'Mozambican metical',
+		numeric: '943',
+	},
+	{
+		code: 'NAD',
+		currency: 'Namibian dollar',
+		numeric: '516',
+	},
+	{
+		code: 'NGN',
+		currency: 'Nigerian naira',
+		numeric: '566',
+	},
+	{
+		code: 'NIO',
+		currency: 'Nicaraguan córdoba',
+		numeric: '558',
+	},
+	{
 		code: 'NOK',
 		currency: 'Norwegian krone',
 		numeric: '578',
 		funding: true,
+	},
+	{
+		code: 'NPR',
+		currency: 'Nepalese rupee',
+		numeric: '524',
 	},
 	{
 		code: 'NZD',
@@ -161,14 +511,49 @@ module.exports = [
 		numeric: '554',
 	},
 	{
+		code: 'OMR',
+		currency: 'Omani rial',
+		numeric: '512',
+	},
+	{
+		code: 'PAB',
+		currency: 'Panamanian balboa',
+		numeric: '590',
+	},
+	{
+		code: 'PEN',
+		currency: 'Peruvian Sol',
+		numeric: '604',
+	},
+	{
+		code: 'PGK',
+		currency: 'Papua New Guinean kina',
+		numeric: '598',
+	},
+	{
 		code: 'PHP',
 		currency: 'Philippine peso',
 		numeric: '608',
 	},
 	{
+		code: 'PKR',
+		currency: 'Pakistani rupee',
+		numeric: '586',
+	},
+	{
 		code: 'PLN',
-		currency: 'Polish zloty',
+		currency: 'Polish złoty',
 		numeric: '985',
+	},
+	{
+		code: 'PYG',
+		currency: 'Paraguayan guaraní',
+		numeric: '600',
+	},
+	{
+		code: 'QAR',
+		currency: 'Qatari riyal',
+		numeric: '634',
 	},
 	{
 		code: 'RON',
@@ -186,9 +571,29 @@ module.exports = [
 		numeric: '643',
 	},
 	{
+		code: 'RWF',
+		currency: 'Rwandan franc',
+		numeric: '646',
+	},
+	{
 		code: 'SAR',
 		currency: 'Saudi riyal',
 		numeric: '682',
+	},
+	{
+		code: 'SBD',
+		currency: 'Solomon Islands dollar',
+		numeric: '090',
+	},
+	{
+		code: 'SCR',
+		currency: 'Seychelles rupee',
+		numeric: '690',
+	},
+	{
+		code: 'SDG',
+		currency: 'Sudanese pound',
+		numeric: '938',
 	},
 	{
 		code: 'SEK',
@@ -202,9 +607,54 @@ module.exports = [
 		numeric: '702',
 	},
 	{
+		code: 'SHP',
+		currency: 'Saint Helena pound',
+		numeric: '654',
+	},
+	{
+		code: 'SLL',
+		currency: 'Sierra Leonean leone',
+		numeric: '694',
+	},
+	{
+		code: 'SOS',
+		currency: 'Somali shilling',
+		numeric: '706',
+	},
+	{
+		code: 'SRD',
+		currency: 'Surinamese dollar',
+		numeric: '968',
+	},
+	{
+		code: 'STD',
+		currency: 'São Tomé and Príncipe dobra',
+		numeric: '678',
+	},
+	{
+		code: 'SYP',
+		currency: 'Syrian pound',
+		numeric: '760',
+	},
+	{
+		code: 'SZL',
+		currency: 'Swazi lilangeni',
+		numeric: '748',
+	},
+	{
 		code: 'THB',
 		currency: 'Thai baht',
 		numeric: '764',
+	},
+	{
+		code: 'TJS',
+		currency: 'Tajikistani somoni',
+		numeric: '972',
+	},
+	{
+		code: 'TMT',
+		currency: 'Turkmenistani manat',
+		numeric: '934',
 	},
 	{
 		code: 'TND',
@@ -212,9 +662,19 @@ module.exports = [
 		numeric: '788',
 	},
 	{
+		code: 'TOP',
+		currency: 'Tongan paʻanga',
+		numeric: '776',
+	},
+	{
 		code: 'TRY',
 		currency: 'Turkish lira',
 		numeric: '949',
+	},
+	{
+		code: 'TTD',
+		currency: 'Trinidad and Tobago dollar',
+		numeric: '780',
 	},
 	{
 		code: 'TWD',
@@ -222,9 +682,19 @@ module.exports = [
 		numeric: '901',
 	},
 	{
+		code: 'TZS',
+		currency: 'Tanzanian shilling',
+		numeric: '834',
+	},
+	{
 		code: 'UAH',
 		currency: 'Ukrainian hryvnia',
 		numeric: '980',
+	},
+	{
+		code: 'UGX',
+		currency: 'Ugandan shilling',
+		numeric: '800',
 	},
 	{
 		code: 'USD',
@@ -233,13 +703,73 @@ module.exports = [
 		funding: true,
 	},
 	{
+		code: 'UYU',
+		currency: 'Uruguayan peso',
+		numeric: '858',
+	},
+	{
+		code: 'UZS',
+		currency: 'Uzbekistan som',
+		numeric: '860',
+	},
+	{
+		code: 'VEF',
+		currency: 'Venezuelan bolívar',
+		numeric: '937',
+	},
+	{
 		code: 'VND',
 		currency: 'Vietnamese dong',
 		numeric: '704',
 	},
 	{
+		code: 'VUV',
+		currency: 'Vanuatu vatu',
+		numeric: '548',
+	},
+	{
+		code: 'WST',
+		currency: 'Samoan tala',
+		numeric: '882',
+	},
+	{
+		code: 'XAF',
+		currency: 'CFA franc BEAC',
+		numeric: '950',
+	},
+	{
+		code: 'XCD',
+		currency: 'East Caribbean dollar',
+		numeric: '951',
+	},
+	{
+		code: 'XOF',
+		currency: 'CFA franc BCEAO',
+		numeric: '952',
+	},
+	{
+		code: 'XPF',
+		currency: 'CFP franc',
+		numeric: '953',
+	},
+	{
+		code: 'YER',
+		currency: 'Yemeni rial',
+		numeric: '886',
+	},
+	{
 		code: 'ZAR',
 		currency: 'South African rand',
 		numeric: '710',
+	},
+	{
+		code: 'ZMK',
+		currency: 'Zambian kwacha',
+		numeric: '894',
+	},
+	{
+		code: 'ZWL',
+		currency: 'Zimbabwean dollar',
+		numeric: '716',
 	},
 ];
